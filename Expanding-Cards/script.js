c35028d8fe6a8c7700a4ee67e40cfb7394fb79db
Amit Kumar Mandal <itsme.amit1230@gmail.com>
2021-05-02 04:46:34 +0530

    Expanding cards on click

---
+++ Expanding-Cards/script.js
@@ -0,0 +1,14 @@
+var cards = document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+    card.addEventListener("click" , () =>{
+        removeActiveClass();
+        card.classList.add('active');
+    })
+})
+
+function removeActiveClass(){
+    cards.forEach(card=>{
+        card.classList.remove('active');
+    })
+}
