23b9ee6944e861c4b3140478581bf14adc4db5dd
Amit Kumar Mandal <itsme.amit1230@gmail.com>
2021-05-24 19:23:12 +0530

    An Awesome Modal

---
+++ Modal/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelector('.show-modal');
+
+
+const openModal = () => {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+
+const closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+
+btnsOpenModal.addEventListener('click', openModal)
+
+
+btnCloseModal.addEventListener('click', closeModal)
+
+overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', (e)=> {
+  if(e.key == 'Escape' && !modal.classList.contains('hidden')) closeModal();
+})
